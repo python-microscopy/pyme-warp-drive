b1cfe0c4bc1a1baefef2529a57778fadcee9dcbb
Andrew Barentine <andrew.barentine@yale.edu>
2016-04-26 10:40:11 -0400

    new repo for gpu localization routine.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='PYMEwarpDrive',
+      version='0.1',
+      description='GPU-accelerated fitting plugin for PYME',
+      url='http://github.com/storborg/funniest',
+      author='Andrew Barentine, David Baddeley, and Fang Huang',
+      author_email='joerglab@gmail.com',
+      license='Good question',
+      packages=['warpDrive'],
+      zip_safe=False)
