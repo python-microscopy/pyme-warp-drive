9bd863fa0604b7c506d8ca952c9272445792e3cd
Andrew Barentine <andrew.barentine@yale.edu>
2016-05-03 19:48:33 -0400

    Added testDriveWarpSpeed to repo. Also, noticed that the gain map is not involved in the image smoothing proccess in the current version. Will update shortly

---
+++ warpDrive/testDriveWarpSpeed.py
@@ -0,0 +1,59 @@
+from warpDrive import *
+import numpy as np
+import scipy.io
+import time
+
+'''Load data, gain, and variance from .mat files'''
+rawdat = np.ascontiguousarray(scipy.io.loadmat('/media/aeb85/060EE6D90EE6C0B3/Users/aeb85/Desktop/matlabFitData/imqd1_300.mat')['imqd1_300'], dtype=np.float32)
+#rawdat = rawdat.astype(np.float32)
+#varmap = np.ascontiguousarray(scipy.io.loadmat('/home/aeb85/PycharmProjects/candidatedetection/TiffsForMatlabPyCUDAcomparison/quadrant1/LargeKernel_12pix/varmap.mat')['varmap'][:,:,299], dtype=np.float32)
+varmap = np.ascontiguousarray(scipy.io.loadmat('/media/aeb85/060EE6D90EE6C0B3/Users/aeb85/Desktop/matlabFitData/varmap.mat')['varmap'], dtype=np.float32)
+gainmap = np.ascontiguousarray(scipy.io.loadmat('/media/aeb85/060EE6D90EE6C0B3/Users/aeb85/Desktop/matlabFitData/gainim.mat')['gainim'], dtype=np.float32)
+
+
+halfFilt = np.int32(6) #set half of the filter size
+dfilter1 = normUnifFilter(2*halfFilt)
+halfFilt2 = np.int32(round(0.5*halfFilt))
+dfilter2 = normUnifFilter(2*halfFilt2)
+
+dshape = (200, 200)
+ditemsize = 4  #each pixel is a float32
+
+
+'''Generate warpDrive'''
+_warpDrive = detector(np.shape(rawdat), rawdat.dtype.itemsize, dfilter1, dfilter2)
+
+_warpDrive.allocateMem()
+_warpDrive.prepvar(varmap, gainmap)
+
+stppwr = 4
+#stppwr = 0
+print 10**stppwr
+#cuda.start_profiler()
+
+
+telapsed = []
+for ind in range(0, 10**stppwr):
+    t0 = time.time()
+    _warpDrive.smoothFrame(rawdat)
+    _warpDrive.getCand(3.7, 16) #adjusted threshold in order to run the same nubmer of fits as Matlab. roi16 sets maxfilt size = 15
+    _warpDrive.fitItSlow(18) #NOTE: need getCand run each loop in order to test fitItSlow because candPos is zerod after fit each run
+    t1 = time.time()
+    telapsed.append(t1-t0)
+
+#chan1.offIt()
+#chan2.offIt()
+
+################################################################################
+################################################################################
+################################################################################
+################################################################################
+
+print('Mean runtime of %.7f, +- %.7f' % (np.mean(telapsed), np.std(telapsed)))
+
+print _warpDrive.dpars.shape
+
+import matplotlib.pyplot as plt
+plt.imshow(rawdat)
+plt.scatter(_warpDrive.dpars[:, 0], _warpDrive.dpars[:, 1])
+plt.show()
